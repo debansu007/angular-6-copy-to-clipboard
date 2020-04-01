08b795e24137b293ded9004e4e31d1649acd193c
debansu007 <debansu.karmakar007@gmail.com>
2020-04-01 13:47:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-6-copy-to-clipboard-ullrl2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-6-copy-to-clipboard-ullrl2)
---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+})
+export class AppComponent  { 
+  name:string;
+
+/* To copy Text from Textbox */
+  copyInputMessage(inputElement){
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.setSelectionRange(0, 0);
+  }
+
+/* To copy any Text */
+copyText(val: string){
+  let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+}
